e04e359807492c0e0ec4a1edc9017c7f58fe22d3
Tornesko <konstantinborovenskij@gmail.com>
2022-07-12 14:54:12 +0300

    init commit

--- atchess/atchess/settings.py
+++ atchess/atchess/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$*1q8ycd!e(z2i^_v7_r@h7l(^!ym2q)i&vfo_^1ts(phw%t$k'
+SECRET_KEY = 'django-insecure-$*1q8ycd!e(z2i^_v7_r@h7rqgerl(^!ym2q)i&vfo_^1ts(phw%t$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
